2e603f986c7a5674e3a3d4f920898d63f8d20459
Shay Zeevi <shay@zogomarket.com>
2020-12-16 13:43:55 +0200

    making another example for underscored named method, please dont use it

--- tests/ExampleClassTest.php
+++ tests/ExampleClassTest.php
@@ -11,8 +11,32 @@ class ExampleClassTest extends TestCase {
       $this->assertEquals('Hello World', $finalResults->getTwoValues());
    }
 
-
+   /**
+    * In this test we are checking that it reterns
+    */
    public function testValuesAreEmptyByDefault() {
+      $finalResults = new ExampleClass();
+      $this->assertEquals('', $finalResults->getTwoValues());
+   }
+
+   /**
+    * Run this test to see what will happened if you are not prefixing the method name with test
+    */
+   public function valuesAreEmptyByDefault() {
+      $finalResults = new ExampleClass();
+      $this->assertEquals('', $finalResults->getTwoValues());
+   }
+
+
+   /*
+    * If you want to run a test without a "test" prefix you can use the next syntax:
+    * words separated by underscores and a phpDoc saying its a test
+    */
+
+   /**
+    * @test
+    */
+   public function values_are_empty_by_default() {
       $finalResults = new ExampleClass();
       $this->assertEquals('', $finalResults->getTwoValues());
    }
